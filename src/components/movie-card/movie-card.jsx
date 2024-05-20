83e9cf82d11970223112e87f37fed0b9166ff956
Orsolya <orsolya.kiss.design@gmail.com>
2024-05-20 15:01:25 -0400

    linked API to React, MainView can fetch and render data

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,17 @@
+import PropTypes from "prop-types";
+import React from "react";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.Title}
+    <div onClick={() => onMovieClick(movie)}>
+      <h3>{movie.Title}</h3> {/* Update to movie.Title */}
     </div>
   );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
