08b22ff672567540201c35fbac0162d5a446adc1
Orsolya <orsolya.kiss.design@gmail.com>
2024-05-20 20:00:03 -0400

    form creation, authentication, API linked

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,70 +1,62 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      _id: "001",
-      Title: "The Dark Knight",
-      Year: "2008",
-      mainActor: {
-        Name: "Christian Bale",
-        Birthday: "1974-01-30",
-      },
-      Genre: {
-        Name: "Action",
-        Description:
-          "The action film is a film genre that predominantly features chase sequences, fights, shootouts, explosions, and stunt work.",
-      },
-      Description:
-        "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
-      ImageURL:
-        "https://resizing.flixster.com/isBlpcooCvG86jAfX46NOqu-dD4=/fit-in/705x460/v2/https://resizing.flixster.com/pf1B_FfX1A1eWM5joLcx-N4q2Os=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2IyMDk1NTJjLTllMjYtNDE1Yy04Yjk2LTk0NGE0MzZiMGQ2NC53ZWJw",
-    },
-    {
-      _id: "002",
-      Title: "The Godfather",
-      Year: "1972",
-      mainActor: {
-        Name: "Marlon Brando",
-        Birthday: "1924-04-03",
-      },
-      Genre: {
-        Name: "Crime",
-        Description:
-          "Crime films, in the broadest sense, is a film genre inspired by and analogous to the crime fiction literary genre. Films of this genre generally involve various aspects of crime and its detection.",
-      },
-      Description:
-        "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
-      ImageURL:
-        "https://resizing.flixster.com/I5729h46fM8XXzaYrT6-j8AlMG0=/300x300/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/NowShowing/43177/43177_aa.jpg",
-    },
-    {
-      _id: "003",
-      Title: "The Revenant",
-      Year: "2015",
-      mainActor: {
-        Name: "Leonardo DiCaprio",
-        Birthday: "1974-11-11",
-      },
-      Genre: {
-        Name: "Action",
-        Description:
-          "The action film is a film genre that predominantly features chase sequences, fights, shootouts, explosions, and stunt work.",
-      },
-      Description:
-        "A frontiersman on a fur trading expedition in the 1820s fights for survival after being mauled by a bear and left for dead by members of his own hunting team.",
-      ImageURL:
-        "https://resizing.flixster.com/_oCXXf24aFzKt-HZUsB9mr3Qt7I=/300x300/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p11598208_k_v8_ac.jpg",
-    },
-  ]);
-
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
+  const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    fetch("https://film-fiesta-2f42541ec594.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((response) => response.json())
+      .then((movies) => {
+        setMovies(movies);
+      });
+  }, [token]);
+
+  if (!user) {
+    return (
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+            localStorage.setItem("user", JSON.stringify(user));
+            localStorage.setItem("token", token);
+          }}
+        />
+        or
+        <SignupView
+          onSignedUp={(user, token) => {
+            setUser(user);
+            setToken(token);
+            localStorage.setItem("user", JSON.stringify(user));
+            localStorage.setItem("token", token);
+          }}
+        />
+      </>
+    );
+  }
 
   const handleMovieClick = (movie) => {
     setSelectedMovie(movie);
   };
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   if (selectedMovie) {
     return (
@@ -88,6 +80,15 @@ export const MainView = () => {
           onMovieClick={handleMovieClick}
         />
       ))}
+      <button
+        onClick={() => {
+          setUser(null);
+          setToken(null);
+          localStorage.clear();
+        }}
+      >
+        Logout
+      </button>
     </div>
   );
 };
