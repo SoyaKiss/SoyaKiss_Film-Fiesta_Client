8f945f80f0455c52b6db6b3050f29570087d6520
Orsolya <orsolya.kiss.design@gmail.com>
2024-06-22 20:59:21 -0400

    DO NOT USE
    DO NOT USE

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,21 +1,37 @@
-import React from "react";
 import PropTypes from "prop-types";
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import "./movie-card.scss";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.Title}
-    </div>
+    <Card className="h-100" style={{ width: "16rem" }}>
+      <div className="card-content">
+        <div className="image-container">
+          <Card.Img variant="top" src={movie.ImageURL} className="image" />
+        </div>
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+        </Card.Body>
+        <div className="button-container">
+          <Button
+            variant="outline-secondary"
+            onClick={() => onMovieClick(movie)}
+            className="button"
+          >
+            More ...
+          </Button>
+        </div>
+      </div>
+    </Card>
   );
 };
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
     ImageURL: PropTypes.string.isRequired,
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired,
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,16 +1,36 @@
-import React from "react";
+import { Button, Card, Container, Col, Row } from "react-bootstrap";
+import "./movie-view.scss";
 
 export const MovieView = ({ movie, onBackClick }) => {
   return (
-    <div>
-      <div>
-        <img src={movie.ImageURL} />
-      </div>
-      <div>
-        <span>Title: </span>
-        <span>{movie.Title}</span>
-      </div>
-      <button onClick={onBackClick}>Back</button>
-    </div>
+    <Container>
+      <Row className="justify-content-center">
+        <Card className="movie-card-container">
+          <Card.Img
+            variant="top"
+            src={movie.ImageURL}
+            className="movie-image-container"
+          />
+          <Card.Body>
+            <Card.Title>{movie.Title}</Card.Title>
+            <Card.Text>Genre: {movie.Genre.Name}</Card.Text>
+            <Card.Text>Main Actor: {movie.mainActor.Name}</Card.Text>
+            <Card.Text>
+              Supporting Actor: {movie.supportingActor.Name}
+            </Card.Text>
+            <Card.Text>Description: {movie.Description}</Card.Text>
+          </Card.Body>
+          <div className="movie-back-button-container">
+            <Button
+              variant="outline-secondary"
+              onClick={onBackClick}
+              className="movie-back-button"
+            >
+              Back
+            </Button>
+          </div>
+        </Card>
+      </Row>
+    </Container>
   );
 };
