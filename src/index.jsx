10c877d21bf05e62426aaf113a8f812b808a37b5
Orsolya <orsolya.kiss.design@gmail.com>
2024-07-06 13:21:20 -0400

    expired token, retake on boostrap

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,13 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
-import "bootstrap/scss/bootstrap.scss";
-import Container from "react-bootstrap/Container";
+
 import "./index.scss";
 
 const FilmFiestaApplication = () => {
-  return (
-    <Container style={{ border: "1px solid red" }}>
-      <MainView />
-    </Container>
-  );
+  return <MainView />;
 };
 
 const container = document.querySelector("#root");
 const root = createRoot(container);
 
-root.render(<MainView />);
+root.render(<FilmFiestaApplication />);
